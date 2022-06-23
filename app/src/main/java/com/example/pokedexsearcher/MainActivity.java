eed5005f4f2a90e5ecb86681280a1950edecb177
Maciej Glowacki <mkglowacki00@gmail.com>
2022-06-23 21:42:36 +0200

    zmiany kolorków i inne bajery

--- app/src/main/java/com/example/pokedexsearcher/MainActivity.java
+++ app/src/main/java/com/example/pokedexsearcher/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.pokedexsearcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,20 +29,24 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+
     private String capitalize(final String line) {
         return Character.toUpperCase(line.charAt(0)) + line.substring(1);
     }
 
-
+    LinearLayout linearLayout_header;
     Button button_search;
     EditText et_dataInput;
-    ConstraintLayout cl_typeA, cl_typeB;
+    ConstraintLayout cl_typeA, cl_typeB, cl_pokeinfo;
     ImageView img_pokemon;
     ScrollView scrollView_pokeInfo;
-    TextView textView_pokemonName, textView_pokemonId, textView_heightValue, textView_weightValue,
-            textView_typeA1, textView_typeB1, textView_typeB2, textView_statHPValue,
+    TextView textView_pokemonName, textView_pokemonId, textView_height, textView_heightValue, textView_weight,
+            textView_weightValue, textView_typeA1, textView_typeB1, textView_typeB2, textView_statHPValue,
             textView_statAttackValue, textView_statDefenseValue, textView_statSpecialAttackValue,
-            textView_statSpecialDefenseValue, textView_statSpeedValue;
+            textView_statSpecialDefenseValue, textView_statSpeedValue, textView_statHP, textView_statAttack,
+            textView_statDefense, textView_statSpecialAttack, textView_statSpecialDefense, textView_statSpeed ;
 
 
     @Override
@@ -47,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cl_pokeinfo = findViewById(R.id.cl_pokeInfo);
+        linearLayout_header = findViewById(R.id.linearLayout_header);
+        textView_height = findViewById(R.id.textView_height);
+        textView_weight = findViewById(R.id.textView_weight);
+        textView_statHP= findViewById(R.id.textView_statHP);
+        textView_statAttack= findViewById(R.id.textView_statAttack);
+        textView_statDefense= findViewById(R.id.textView_statDefense);
+        textView_statSpecialAttack= findViewById(R.id.textView_statSpecialAttack);
+        textView_statSpecialDefense= findViewById(R.id.textView_statSpecialDefense);
+        textView_statSpeed= findViewById(R.id.textView_statSpeed);
         button_search = findViewById(R.id.button_search);
         et_dataInput = findViewById(R.id.et_dataInput);
         scrollView_pokeInfo = findViewById(R.id.scrollView_pokeInfo);
@@ -100,12 +117,21 @@ public class MainActivity extends AppCompatActivity {
                         textView_pokemonId.setText(pokemon_idText);
 
                         scrollView_pokeInfo.setVisibility(View.VISIBLE);
+
+                        String backgroundColor, textColor;
+                        PokedexColors pokedexColors;
+
                         if(pokemon.isManyTypes()){
 
                             cl_typeA.setVisibility(View.INVISIBLE);
                             cl_typeB.setVisibility(View.VISIBLE);
                             textView_typeB1.setText(capitalize(pokemon.typeB1));
                             textView_typeB2.setText(capitalize(pokemon.typeB2));
+
+                            pokedexColors = new PokedexColors(pokemon.getTypeB1(), pokemon.getTypeB2());
+
+                            textView_typeB2.setTextColor(Color.parseColor(pokedexColors.getSecondaryColorHexValue()));
+                            textView_typeB1.setTextColor(Color.parseColor(pokedexColors.getMainTypeHexValue()));
 
 
                         }else{
@@ -114,11 +140,39 @@ public class MainActivity extends AppCompatActivity {
                             textView_typeA1.setText(capitalize(pokemon.typeA));
                             cl_typeB.setVisibility(View.INVISIBLE);
 
+                            pokedexColors = new PokedexColors(pokemon.getTypeA());
+
+                            textView_typeA1.setTextColor(Color.parseColor(pokedexColors.getSecondaryColorHexValue()));
+
                         }
 
+                        backgroundColor = pokedexColors.getMainColorHexValue();
+                        textColor = pokedexColors.getSecondaryColorHexValue();
 
-                        textView_heightValue.setText(Double.toString(pokemon.getHeight()) );
-                        textView_weightValue.setText(Double.toString(pokemon.getWeight()) );
+                        cl_pokeinfo.setBackgroundColor(Color.parseColor(backgroundColor));
+                        linearLayout_header.setBackgroundColor(Color.parseColor(backgroundColor));
+                        button_search.setBackgroundColor(Color.parseColor(textColor));
+
+                        textView_pokemonName.setTextColor(Color.parseColor(textColor));
+                        textView_pokemonId.setTextColor(Color.parseColor(textColor));
+                        textView_height.setTextColor(Color.parseColor(textColor));
+                        textView_weight.setTextColor(Color.parseColor(textColor));
+                        textView_statHP.setTextColor(Color.parseColor(textColor));
+                        textView_statAttack.setTextColor(Color.parseColor(textColor));
+                        textView_statDefense.setTextColor(Color.parseColor(textColor));
+                        textView_statSpecialAttack.setTextColor(Color.parseColor(textColor));
+                        textView_statSpecialDefense.setTextColor(Color.parseColor(textColor));
+                        textView_statSpeed.setTextColor(Color.parseColor(textColor));
+
+
+
+                        Toast.makeText(MainActivity.this, backgroundColor, Toast.LENGTH_LONG).show();
+
+
+
+
+                        textView_heightValue.setText(Double.toString(pokemon.getHeight()) + "m" );
+                        textView_weightValue.setText(Double.toString(pokemon.getWeight()) + "kg");
 
                         textView_statHPValue.setText(Integer.toString(pokemon.getHp()));
                         textView_statAttackValue.setText(Integer.toString(pokemon.getAttack()));
---
+++ app/src/main/java/com/example/pokedexsearcher/PokedexColors.java
@@ -0,0 +1,88 @@
+package com.example.pokedexsearcher;
+
+import java.util.ArrayList;
+
+public class PokedexColors {
+
+    String mainTypeColor, secondaryTypeColor;
+    String mainColorHexValue, secondaryColorHexValue, mainTypeHexValue;
+
+    String[][] colors ={{"normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison",
+            "ground", "flying", "psychic", "bug", "rock", "ghost", "dragon", "dark", "steel", "fairy"},
+            {"#A8A77A","#EE8130", "#6390F0", "#F7D02C", "#7AC74C", "#96D9D6", "#C22E28", "#A33EA1",
+                    "#E2BF65", "#A98FF3", "#F95587", "#A6B91A", "#B6A136", "#735797", "#6F35FC", "#705746", "#B7B7CE", "#D685AD"},
+            {"#EAE9CE", "#FABB8C", "#C1D1F1", "#FFF1B4", "#CEF8B5", "#E1FAF9", "#FF8480", "#FBA2FA", "#FFE9AF",
+                    "#E0D5FF", "#FFA8C3", "#F2FBAE", "#FFF4BC", "#B79ED8", "#BCA1FF", "#D5BBA9", "#F2F2FB", "#FAC9E1"}};
+
+    public String getMainTypeHexValue() {
+        return mainTypeHexValue;
+    }
+
+    public void setMainTypeHexValue(String mainTypeHexValue) {
+        this.mainTypeHexValue = mainTypeHexValue;
+    }
+
+    public PokedexColors(String mainTypeColor, String secondaryTypeColor) {
+        this.mainTypeColor = mainTypeColor;
+        this.secondaryTypeColor = secondaryTypeColor;
+        setColorHexValue(true, this.mainTypeColor);
+        setColorHexValue(false, this.secondaryTypeColor);
+    }
+
+    public PokedexColors(String mainTypeColor) {
+        this.mainTypeColor = mainTypeColor;
+        this.secondaryTypeColor = mainTypeColor;
+        setColorHexValue(true, this.mainTypeColor);
+        setColorHexValue(false, this.secondaryTypeColor);
+    }
+
+    public void setColorHexValue(boolean isMain, String typeColor){
+
+        for (int i=0; i < 18; i++){
+            if (colors[0][i].equals(typeColor)){
+                if (isMain){
+                    mainColorHexValue = colors[2][i];
+                    mainTypeHexValue = colors[1][i];
+                    break;
+                }else if(!isMain){
+                    secondaryColorHexValue = colors[1][i];
+                }
+            }
+        }
+
+    }
+
+
+
+    public String getMainTypeColor() {
+        return mainTypeColor;
+    }
+
+    public void setMainTypeColor(String mainTypeColor) {
+        this.mainTypeColor = mainTypeColor;
+    }
+
+    public String getSecondaryTypeColor() {
+        return secondaryTypeColor;
+    }
+
+    public void setSecondaryTypeColor(String secondaryTypeColor) {
+        this.secondaryTypeColor = secondaryTypeColor;
+    }
+
+    public String getMainColorHexValue() {
+        return mainColorHexValue;
+    }
+
+    public void setMainColorHexValue(String mainColorHexValue) {
+        this.mainColorHexValue = mainColorHexValue;
+    }
+
+    public String getSecondaryColorHexValue() {
+        return secondaryColorHexValue;
+    }
+
+    public void setSecondaryColorHexValue(String secondaryColorHexValue) {
+        this.secondaryColorHexValue = secondaryColorHexValue;
+    }
+}
